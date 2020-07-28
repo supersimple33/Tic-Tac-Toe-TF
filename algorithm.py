82a36e56871b15e305d11ee44f657ef11370e5b2
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-07-28 16:02:38 -0400

    Added a smart game board

---
+++ algorithm.py
@@ -0,0 +1,12 @@
+import game
+# import random
+# import sys
+
+# A subclass of game that has an added method for making the best/smartest move
+class SmartGame(game.Game):
+    def smartMove():
+        if self.canPlay(4):
+            self.play(4)
+        else:
+            #do something smart
+            print() # placeholder
