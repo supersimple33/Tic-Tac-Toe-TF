cc82af227cb765170e6b63408a38d11e0a60ac23
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-08-05 21:58:06 -0400

    Reformatted TF to use binary data

--- trainer.py
+++ trainer.py
@@ -24,11 +24,11 @@ print(dataset)
 #     print(x,y)
 
 def get_compiled_model():
-    model = tf.keras.Sequential([tf.keras.layers.Dense(15, activation='relu', input_shape=(9, )),tf.keras.layers.Dense(20, activation='relu'),tf.keras.layers.Dense(15, activation='relu'),tf.keras.layers.Dense(9)])
+    model = tf.keras.Sequential([tf.keras.layers.Dense(21, activation='relu', input_shape=(27, )),tf.keras.layers.Dense(15, activation='relu'),tf.keras.layers.Dense(9)])#tf.keras.layers.Dense(15, activation='relu')
     model.compile(optimizer='rmsprop', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
     return model
 
 model = get_compiled_model()
-model.fit(train_dataset, epochs=30)
+model.fit(train_dataset, epochs=15)
 model.save('saved_model/my_model')
 print(model.summary())
