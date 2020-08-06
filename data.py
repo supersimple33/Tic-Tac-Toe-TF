c77474a8c852f4cb232c3bbd55470829069bc6fc
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-08-05 21:53:44 -0400

    Spaces are now saved in binary

--- data.py
+++ data.py
@@ -36,13 +36,13 @@ def playGame():
 		pmoves.remove(move)
 	del b
 
-gameRepo = pd.DataFrame(columns=['move', "0s", "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s"])
+gameRepo = pd.DataFrame(columns=['move', "0o", "1o", "2o", "3o", "4o", "5o", "6o", "7o", "8o", "0e", "1e", "2e", "3e", "4e", "5e", "6e", "7e", "8e", "0x", "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x"])
 
 for i in range(10):
 	g = playGame()
 	if g != None:
 		for gameState in g.items():
-			formedReplay = {"move" : gameState[0], "0s": gameState[1][0], "1s": gameState[1][1], "2s": gameState[1][2], "3s": gameState[1][3], "4s": gameState[1][4], "5s": gameState[1][5], "6s": gameState[1][6], "7s": gameState[1][7], "8s": gameState[1][8]}
+			formedReplay = {"move" : gameState[0], "0o": gameState[1][0] == -1, "1o": gameState[1][1] == -1, "2o": gameState[1][2] == -1, "3o": gameState[1][3] == -1, "4o": gameState[1][4] == -1, "5o": gameState[1][5] == -1, "6o": gameState[1][6] == -1, "7o": gameState[1][7] == -1, "8o": gameState[1][8] == -1, "0e": gameState[1][0] == 0, "1e": gameState[1][1] == 0, "2e": gameState[1][2] == 0, "3e": gameState[1][3] == 0, "4e": gameState[1][4] == 0, "5e": gameState[1][5] == 0, "6e": gameState[1][6] == 0, "7e": gameState[1][7] == 0, "8e": gameState[1][8] == 0, "0x": gameState[1][0] == 1, "1x": gameState[1][1] == 1, "2x": gameState[1][2] == 1, "3x": gameState[1][3] == 1, "4x": gameState[1][4] == 1, "5x": gameState[1][5] == 1, "6x": gameState[1][6] == 1, "7x": gameState[1][7] == 1, "8x": gameState[1][8] == 1}
 			# formedReplay = [gameState[0], gameState[1][0], gameState[1][1], gameState[1][2], gameState[1][3], gameState[1][4], gameState[1][5], gameState[1][6], gameState[1][7], gameState[1][8]}
 			gameRepo = gameRepo.append(formedReplay, ignore_index=True)
 	if i % 1000 == 0:
@@ -53,7 +53,7 @@ print("Begin Saving")
 # 	json.dump(gameRepo, fp)
 # 	print("Saved %d moves to json" % len(gameRepo))
 
-gameRepo.drop_duplicates(subset = ["0s", "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s"], keep = 'first', inplace = True)
+gameRepo.drop_duplicates(subset = ["0o", "1o", "2o", "3o", "4o", "5o", "6o", "7o", "8o", "0e", "1e", "2e", "3e", "4e", "5e", "6e", "7e", "8e", "0x", "1x", "2x", "3x", "4x", "5x", "6x", "7x", "8x"], keep = 'first', inplace = True)
 print(gameRepo)
 
 # pd.sort_values("move", inplace=True)
